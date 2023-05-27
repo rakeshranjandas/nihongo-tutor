9391c81aa82ca9952d0a6851707032250fb60d25
rakeshranjandas <rakeshranjandas.bbsr93@gmail.com>
2023-05-27 10:03:26 +0530

    Katakana sound correction - FU

--- kana/library_of_congress/katakana.js
+++ kana/library_of_congress/katakana.js
@@ -46,7 +46,7 @@ export const katakana =
 ヒ,HI,Katakana letter HI
 ビ,BI,Katakana letter BI
 ピ,PI,Katakana letter PI
-フ,HU,Katakana letter HU
+フ,HU,Katakana letter FU
 ブ,BU,Katakana letter BU
 プ,PU,Katakana letter PU
 ヘ,HE,Katakana letter HE
