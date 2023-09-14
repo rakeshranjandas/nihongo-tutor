93efab0d1d957184ddea25796c02256ecf44a876
rakeshranjandas <rakeshranjandas.bbsr93@gmail.com>
2023-09-14 20:01:47 +0530

    Updated vocabulary for Chapter 31

---
+++ vocabulary/chapters/js/31.js
@@ -0,0 +1,44 @@
+export const data = 
+
+`はじまります _ 始まります _ [a ceremony] begin, start
+つづけます _ 続けます _ continue
+みつけます _ 見つけます _ find
+うけます _ [しけんを~] 受けます _ [試験を~] take [an examination]
+にゅうがくします _ [だいがくに~] 入学します _ [大学に~] enter [a university]
+そつぎょうします _ [だいがくを~] 卒業します _ [大学を~] graduate [from a university]
+しゅっせきします _ [かいぎに~] 出席します _ [会議に~] attend [a meeting]
+きゅうけいします _ 休憩します _ take a break, take a rest
+れんきゅう _ 連休 _ consecutive holidays
+さくぶん _ 作文 _ essay, composition
+てんらんかい _ 展覧会 _ exhibition
+けっこんしき _ 結婚式 _ wedding ceremony
+[お]そうしき _ [お]葬式 _ funeral
+しき _ 式 _ ceremony
+ほんしゃ _ 本社 _ head office
+してん _ 支店 _ branch office
+きょうかい _ 教会 _ church
+だいがくいん _ 大学院 _ graduate school
+どうぶつえん _ 動物園 _ zoo
+おんせん _ 温泉 _ hot springs, spa
+おきゃく[さん] _ お客[さん] _ visitor, guest, customer, client
+だれか _ 誰か _ somebody
+~の ほう _ ~の 方 _ place toward ~, direction of ~
+ずっと _ _ all the time
+のこります _ 残ります _ remain, be left
+つきに _ 月に _ per month
+ふつうの _ 普通の _ ordinary, common, usual
+インターネット _ _Internet
+むら _ 村 _ village
+えいがかん _ 映画館 _ movie theater
+いや[な] _ 嫌[な] _ hateful, disagreeable
+そら _ 空 _ sky
+とじます _ 閉じます _ close
+とかい _ 都会 _ city
+こどもたち _ 子どもたち _ children
+じゆうに _ 自由に _ freely
+せかいじゅう _ 世界中 _ all over the world
+あつまります _ 集まります _ gather
+うつくしい _ 美しい _ beautiful
+しぜん _ 自然 _ nature
+すばらしい _ 素晴らしい _ wonderful, magnificent
+きがつきます _ 気がつきます _ notice, become aware of`
