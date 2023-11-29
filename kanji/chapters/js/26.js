69061ebba0f63c9927883ba43cac5028edf9d9b6
rakeshranjandas <rakeshranjandas.bbsr93@gmail.com>
2023-11-29 09:42:10 +0530

    Fix Kanji

--- kanji/chapters/js/26.js
+++ kanji/chapters/js/26.js
@@ -4,11 +4,11 @@ export const data =
 Summer: 夏_なつ
 Autumn: 秋_あき
 Winter: 冬_ふゆ
-Hot (for climate): 暑い_あつい
+Hot (for climate): 暑_あつい
 Hot (for things): 熱_あつい, ねつ
-Cold (for climate): 寒い_さむい
-Cold (for things): 冷たい_つめたい
-Warm (for climate): 暖かい_あたたかい
-Warm (for things): 温泉_おん, あたたかい
-Cool: 涼しい_すずしい
+Cold (for climate): 寒_さむい
+Cold (for things): 冷_つめたい
+Warm (for climate): 暖_あたたかい
+Warm (for things): 温_おん, あたたかい
+Cool: 涼_すずしい
 Weather: 天_てんき`
