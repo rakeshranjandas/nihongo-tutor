f91e001dc4c186b27f6f490b2d16626ba68775c1
rakeshranjandas <rakeshranjandas.bbsr93@gmail.com>
2023-11-29 09:18:41 +0530

    Added Kanji chapter 26

---
+++ kanji/chapters/js/26.js
@@ -0,0 +1,14 @@
+export const data = 
+
+`Spring: 春_はる
+Summer: 夏_なつ
+Autumn: 秋_あき
+Winter: 冬_ふゆ
+Hot (for climate): 暑い_あつい
+Hot (for things): 熱_あつい, ねつ
+Cold (for climate): 寒い_さむい
+Cold (for things): 冷たい_つめたい
+Warm (for climate): 暖かい_あたたかい
+Warm (for things): 温泉_おん, あたたかい
+Cool: 涼しい_すずしい
+Weather: 天_てんき`
